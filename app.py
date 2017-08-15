8e058cede5ed032726a2433568a875dbca9a291a
Henry <henry@rhodochrosite.xyz>
2017-08-15 17:20:33 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
-from flask import Flask, g, session, redirect, request, url_for, jsonify
-from requests_oauthlib import OAuth2Session
+from urllib.parse import urlencode
+
+import aiohttp
+from kyoukai import Kyoukai
+from kyoukai.util import as_json
+from kyoukai.asphalt import HTTPRequestContext, Response
+from werkzeug.utils import redirect
 
 OAUTH2_CLIENT_ID = os.environ['OAUTH2_CLIENT_ID']
 OAUTH2_CLIENT_SECRET = os.environ['OAUTH2_CLIENT_SECRET']
@@ -10,65 +15,51 @@ API_BASE_URL = os.environ.get('API_BASE_URL', 'https://discordapp.com/api')
 AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
 TOKEN_URL = API_BASE_URL + '/oauth2/token'
 
-app = Flask(__name__)
+app = Kyoukai(__name__)
 app.debug = True
-app.config['SECRET_KEY'] = OAUTH2_CLIENT_SECRET
 
-if 'http://' in OAUTH2_REDIRECT_URI:
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+session = aiohttp.ClientSession()
 
 
-def token_updater(token):
-    session['oauth2_token'] = token
+async def fetch_token(code):
+    data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": OAUTH2_REDIRECT_URI,
+        "client_id": OAUTH2_CLIENT_ID,
+        "client_secret": OAUTH2_CLIENT_SECRET
+    }
+
+    response = await session.post(
+        f"{TOKEN_URL}?{urlencode(data)}",
+    )
+
+    js = await response.json()
+    return js["token"]
 
 
-def make_session(token=None, state=None, scope=None):
-    return OAuth2Session(
-        client_id=OAUTH2_CLIENT_ID,
-        token=token,
-        state=state,
-        scope=scope,
-        redirect_uri=OAUTH2_REDIRECT_URI,
-        auto_refresh_kwargs={
-            'client_id': OAUTH2_CLIENT_ID,
-            'client_secret': OAUTH2_CLIENT_SECRET,
-        },
-        auto_refresh_url=TOKEN_URL,
-        token_updater=token_updater)
+@app.route('/', methods=["GET"])
+async def index(ctx: HTTPRequestContext):
+    data = {
+        "scope": "identify email connections guilds guilds.join",
+        "client_id": OAUTH2_CLIENT_ID,
+        "response_type": "code"
+    }
+    return redirect(f"{AUTHORIZATION_BASE_URL}?{urlencode(data)}")
 
 
-@app.route('/')
-def index():
-    scope = request.args.get(
-        'scope',
-        'identify email connections guilds guilds.join')
-    discord = make_session(scope=scope.split(' '))
-    authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
-    session['oauth2_state'] = state
-    return redirect(authorization_url)
+@app.route('/callback', methods=["GET"])
+async def callback(ctx: HTTPRequestContext):
+    code = ctx.request.args["code"]
+    token = await fetch_token(code)
+    return redirect(f'/me?token={token}')
 
 
-@app.route('/callback')
-def callback():
-    if request.values.get('error'):
-        return request.values['error']
-    discord = make_session(state=session.get('oauth2_state'))
-    token = discord.fetch_token(
-        TOKEN_URL,
-        client_secret=OAUTH2_CLIENT_SECRET,
-        authorization_response=request.url)
-    session['oauth2_token'] = token
-    return redirect(url_for('.me'))
-
-
-@app.route('/me')
-def me():
-    discord = make_session(token=session.get('oauth2_token'))
-    user = discord.get(API_BASE_URL + '/users/@me').json()
-    guilds = discord.get(API_BASE_URL + '/users/@me/guilds').json()
-    connections = discord.get(API_BASE_URL + '/users/@me/connections').json()
-    return jsonify(user=user, guilds=guilds, connections=connections)
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/me', methods=["GET"])
+async def me(ctx: HTTPRequestContext):
+    token = ctx.request.args["token"]
+    headers = {"Authorization": f"Bearer {token}"}
+    user = session.get(f"{API_BASE_URL}/users/@me", headers=headers)
+    guilds = session.get(f"{API_BASE_URL}/users/@me/guilds", headers=headers)
+    connections = session.get(f"{API_BASE_URL}/users/@me/connections", headers=headers)
+    return as_json(dict(user=user, guilds=guilds, connections=connections))
