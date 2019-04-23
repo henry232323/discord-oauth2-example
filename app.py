f47531ef50c539387190368e104de80b1fb6226b
Henry <henry@typheus.me>
2019-04-23 17:14:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
+import json
 from urllib.parse import urlencode
 
-import aiohttp
-from kyoukai import Kyoukai
-from kyoukai.util import as_json
-from kyoukai.asphalt import HTTPRequestContext
-from werkzeug.utils import redirect
+from aiohttp import web
+from aiohttp import ClientSession
 
 OAUTH2_CLIENT_ID = os.environ['OAUTH2_CLIENT_ID']
 OAUTH2_CLIENT_SECRET = os.environ['OAUTH2_CLIENT_SECRET']
@@ -15,51 +13,55 @@ API_BASE_URL = os.environ.get('API_BASE_URL', 'https://discordapp.com/api')
 AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
 TOKEN_URL = API_BASE_URL + '/oauth2/token'
 
-app = Kyoukai(__name__)
-app.debug = True
-
-session = aiohttp.ClientSession()
+session = ClientSession()
 
 
-async def fetch_token(code):
-    data = {
-        "code": code,
-        "grant_type": "authorization_code",
-        "redirect_uri": OAUTH2_REDIRECT_URI,
-        "client_id": OAUTH2_CLIENT_ID,
-        "client_secret": OAUTH2_CLIENT_SECRET
-    }
+class OAuthPage(web.Application):
+    def __init__(self):
+        super().__init__()
 
-    response = await session.post(
-        f"{TOKEN_URL}?{urlencode(data)}",
-    )
+        self.add_routes([
+            web.get('/', self.index),
+            web.get('/callback', self.callback),
+            web.get('/me', self.me),
+        ])
 
-    js = await response.json()
-    return js["token"]
+    async def fetch_token(self, code):
+        data = {
+            "code": code,
+            "grant_type": "authorization_code",
+            "redirect_uri": OAUTH2_REDIRECT_URI,
+            "client_id": OAUTH2_CLIENT_ID,
+            "client_secret": OAUTH2_CLIENT_SECRET
+        }
 
+        response = await session.post(
+            f"{TOKEN_URL}?{urlencode(data)}",
+        )
 
-@app.route('/', methods=["GET"])
-async def index(ctx: HTTPRequestContext):
-    data = {
-        "scope": "identify email connections guilds guilds.join",
-        "client_id": OAUTH2_CLIENT_ID,
-        "response_type": "code"
-    }
-    return redirect(f"{AUTHORIZATION_BASE_URL}?{urlencode(data)}")
+        js = await response.json()
+        return js["token"]
 
+    async def index(self, request: web.Request):
+        data = {
+            "scope": "identify email connections guilds guilds.join",
+            "client_id": OAUTH2_CLIENT_ID,
+            "response_type": "code"
+        }
+        raise web.HTTPFound(f"{AUTHORIZATION_BASE_URL}?{urlencode(data)}")
 
-@app.route('/callback', methods=["GET"])
-async def callback(ctx: HTTPRequestContext):
-    code = ctx.request.args["code"]
-    token = await fetch_token(code)
-    return redirect(f'/me?token={token}')
+    async def callback(self, request: web.Request):
+        code = request.query["code"]
+        token = await self.fetch_token(code)
+        raise web.HTTPFound(f'/me?token={token}')
+    
+    async def me(self, request: web.Request):
+        token = request.query["token"]
+        headers = {"Authorization": f"Bearer {token}"}
+        user = session.get(f"{API_BASE_URL}/users/@me", headers=headers)
+        guilds = session.get(f"{API_BASE_URL}/users/@me/guilds", headers=headers)
+        connections = session.get(f"{API_BASE_URL}/users/@me/connections", headers=headers)
 
-
-@app.route('/me', methods=["GET"])
-async def me(ctx: HTTPRequestContext):
-    token = ctx.request.args["token"]
-    headers = {"Authorization": f"Bearer {token}"}
-    user = session.get(f"{API_BASE_URL}/users/@me", headers=headers)
-    guilds = session.get(f"{API_BASE_URL}/users/@me/guilds", headers=headers)
-    connections = session.get(f"{API_BASE_URL}/users/@me/connections", headers=headers)
-    return as_json(dict(user=user, guilds=guilds, connections=connections))
+        response = web.Response(body=json.dumps(dict(user=user, guilds=guilds, connections=connections)))
+        response.headers['content-type'] = 'text/json'
+        return response
